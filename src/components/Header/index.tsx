60b221028d69893ee48a41eec1f33b47fdc81403
Matheus Martins <matheusms280100@gmail.com>
2023-02-08 18:48:12 -0300

    add header styles and phosphor-icons

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,21 @@
+import { Timer, Scroll } from 'phosphor-react'
+import { HeaderContainer } from './styles'
+
+import logo from '../../assets/logo.svg'
+import { NavLink } from 'react-router-dom'
+
+export function Header() {
+  return (
+    <HeaderContainer>
+      <img src={logo} alt="" />
+      <nav>
+        <NavLink to="/">
+          <Timer size={24} />
+        </NavLink>
+        <NavLink to="/history">
+          <Scroll size={24} />
+        </NavLink>
+      </nav>
+    </HeaderContainer>
+  )
+}
