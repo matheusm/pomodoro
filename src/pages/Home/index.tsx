94f8358720326a348d1804783b418a168ffe5787
Matheus Martins <matheusms280100@gmail.com>
2023-02-12 21:02:20 -0300

    add home page styles

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,7 +3,10 @@ import {
   CountDownContainer,
   FormContainer,
   HomeContainer,
+  MinutesAmounInput,
   Separator,
+  StartContDownButton,
+  TaskInput,
 } from './styles'
 
 export function Home() {
@@ -12,10 +15,27 @@ export function Home() {
       <form action="">
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
-          <input id="task" />
+          <TaskInput
+            id="task"
+            list="task-suggestions"
+            placeholder="De um nome para o seu projeto"
+          />
+
+          <datalist id="task-suggestions">
+            <option value="projeto 1" />
+            <option value="projeto 2" />
+            <option value="projeto 3" />
+          </datalist>
 
           <label htmlFor="minutesAmount">durante</label>
-          <input type="number" id="minutesAmount" />
+          <MinutesAmounInput
+            type="number"
+            id="minutesAmount"
+            placeholder="00"
+            step={5}
+            max={60}
+            min={0}
+          />
 
           <span>minutos.</span>
         </FormContainer>
@@ -28,10 +48,10 @@ export function Home() {
           <span>0</span>
         </CountDownContainer>
 
-        <button type="submit">
+        <StartContDownButton type="submit">
           <Play size={24} />
           Começar
-        </button>
+        </StartContDownButton>
       </form>
     </HomeContainer>
   )
