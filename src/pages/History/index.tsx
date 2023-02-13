6f7308a9beb8196b8b34fd9b56c47e350273b3ae
Matheus Martins <matheusms280100@gmail.com>
2023-02-12 21:14:13 -0300

    add base history page

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,3 +1,66 @@
+import { HistoryContainer, HistoryList } from './styles'
+
 export function History() {
-  return <h1>history</h1>
+  return (
+    <HistoryContainer>
+      <h1>Meu histórico</h1>
+
+      <HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>Início</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Ha certa de 2 meses</td>
+              <td>Em andamento</td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Ha certa de 2 meses</td>
+              <td>Em andamento</td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Ha certa de 2 meses</td>
+              <td>Em andamento</td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Ha certa de 2 meses</td>
+              <td>Em andamento</td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Ha certa de 2 meses</td>
+              <td>Em andamento</td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Ha certa de 2 meses</td>
+              <td>Em andamento</td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Ha certa de 2 meses</td>
+              <td>Em andamento</td>
+            </tr>
+          </tbody>
+        </table>
+      </HistoryList>
+    </HistoryContainer>
+  )
 }
