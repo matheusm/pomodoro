59fef671ecf3f92e925d7f3d9ab79d285d8b46c8
Matheus Martins <matheusms280100@gmail.com>
2023-02-15 15:04:09 -0300

    reset timer when start a new cycle

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -45,12 +45,18 @@ export function Home() {
   const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
 
   useEffect(() => {
+    let interval: number
+
     if (activeCycle) {
-      setInterval(() => {
+      interval = setInterval(() => {
         setAmountSecondsPassed(
           differenceInSeconds(new Date(), activeCycle.startDate),
         )
       }, 1000)
+    }
+
+    return () => {
+      clearInterval(interval)
     }
   }, [activeCycle])
 
@@ -66,6 +72,7 @@ export function Home() {
 
     setCycles((state) => [...state, newCycle])
     setActiveCycleId(id)
+    setAmountSecondsPassed(0)
 
     reset()
   }
